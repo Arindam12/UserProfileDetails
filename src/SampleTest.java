be2755eb76f0320432e2aead6a182a3142b2e49e
Arindam12 <arindamtarafdar@hotmail.com>
2015-12-30 09:25:48 +0530

    changes made for git repos

---
+++ src/SampleTest.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+		
+		ArrayList ar=new ArrayList();
+		
+	ar.add(1);
+	ar.add(2);
+	Iterator ir=ar.iterator();
+	
+	while(ir.hasNext())
+		
+		System.out.println(ir.next());
+	
+	
+	}
+
+}
